3d97072eb677da171b3cbc0bf14943e69d821c35
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2024-12-04 10:36:19 +0700

    Update key.js

--- key.js
+++ key.js
@@ -1,4 +1,4 @@
-global.username = "Rerezz"
+global.username = "Rerez"
 global.password = "0208"
 global.konek = "6285216955233"
 global.salurann = "https://whatsapp.com/channel/0029Vat4lwpKLaHoS0MAwj3Z"
